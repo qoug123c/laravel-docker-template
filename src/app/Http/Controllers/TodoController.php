42274b62276b87ff102f960eda61a6898ad79f90
BW <cotsuka@ChienoMacBook-Pro.local>
2026-08-14 11:31:09 +0900

    add:todo/show.blade.php, update:TodoController.php,index.blade.php,routes/web.php【sc16】詳細画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,11 +14,13 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+
     public function create()
     {
         // TODO: 第1引数を指定
         return view('todo.create');
     }
+
     public function store(Request $request)
     {
         $inputs = $request->all();
@@ -28,5 +30,13 @@ class TodoController extends Controller
         $todo->save();
 
         return redirect()->route('todo.index');
+    }
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
     }
 }
